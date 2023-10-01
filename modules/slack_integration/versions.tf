d0cfe0a197d60708c3e9b50b15da0e2e2f4f9214
Louis Fiori <56321138+0nwerd@users.noreply.github.com>
2023-10-01 17:57:50 +0200

    👨‍💻 Update versions

--- modules/slack_integration/versions.tf
+++ modules/slack_integration/versions.tf
@@ -1,10 +1,20 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = "~> 1.4.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.0"
+    }
+
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 0.61"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
     }
   }
 }
