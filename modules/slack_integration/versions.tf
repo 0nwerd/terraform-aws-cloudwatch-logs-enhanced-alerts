a9e72b4b5dc47388afe3a5dcb3fad0002dcd0b12
Louis Fiori <56321138+0nwerd@users.noreply.github.com>
2023-10-04 15:59:11 +0200

    Fix #1 (Update)

--- modules/slack_integration/versions.tf
+++ modules/slack_integration/versions.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = "> 1.4.5"
+  required_version = ">= 1.4.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "> 4.0.0"
+      version = ">= 4.0.0"
     }
 
     awscc = {
       source  = "hashicorp/awscc"
-      version = "> 0.2"
+      version = ">= 0.2"
     }
   }
 }
